0aa62b3eb145ea608931ff6bcbd064d1665355bd
Erna Lukac <e.lukac4@gmail.com>
2022-01-17 21:36:03 -0700

    layout for the page in HTML is complete, next up to bat, JS, on deck - CSS

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+// Auto complete for search bar -- not currently working
+
+// $(function () {
+//     let availableTags = [
+//         "Denver",
+//         "New York",
+//         "Seattle",
+//     ];
+//     $(".autocomplete").autocomplete({
+//         source: availableTags
+//     });
+// )};
+
