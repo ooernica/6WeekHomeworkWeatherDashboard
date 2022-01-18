b2e12d101598f6d8acbfe46191e69d97e2158bd0
Erna Lukac <e.lukac4@gmail.com>
2022-01-18 14:08:15 -0700

    attempt at API Call

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,24 @@
-// Auto complete for search bar -- not currently working
+// Variables
+let requestUrl = 'https://openweathermap.org/api/one-call-api';
+
+let resonseText = document.getElementById('BLA');
+
+// API Call
+fetch(requestUrl)
+    .then (function (response) {
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    })
+
+   
+
+
+
+
+
+// Auto complete for search bar -- not currently working, come back to this later
 
 // $(function () {
 //     let availableTags = [
@@ -10,4 +30,3 @@
 //         source: availableTags
 //     });
 // )};
-
